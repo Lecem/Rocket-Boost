b07320a8b1c6e9e91b2cec0cd4c145c342c83683
Lecem <lecem.gokal@gmail.com>
2025-02-06 10:38:17 +0300

    Elevator is doing animation with DOTween

--- Assets/Scripts/TweenEducation.cs
+++ Assets/Scripts/TweenEducation.cs
@@ -18,10 +18,10 @@ public class TweenEducation : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space))
         {
            // var targetPos =new Vector3(Elevatorr.position.x,Elevatorr.position.y+30,Elevatorr.position.z);
-           //Elevatorr.DOMove(targetPos, 5).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Linear) ;
+          // Elevatorr.DOMove(new Vector3(hedefPozisyon.position.x,hedefPozisyon.position.y,hedefPozisyon.position.z ), 5).SetLoops(-1,LoopType.Yoyo).SetEase(Ease.Linear) ;
 
-           Elevatorr.DOMoveY( 26f, 5f ).SetEase(barrelEase);
-
+           Elevatorr.DOMoveY( 26f, 5f ).SetLoops( -1, LoopType.Yoyo).SetEase(barrelEase);
+           //burada SetEase(barrelEase) yapmazsak unity üzerinden yaptığımız animasyon seçimi farketmez.
         }
     }
 }
